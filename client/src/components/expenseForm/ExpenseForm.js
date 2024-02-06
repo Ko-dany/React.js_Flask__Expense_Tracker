d6c5e40e3e45604b3da7853b62de1df18170e1fa
Dahyun (Dany) <iamgo910@gmail.com>
2024-02-06 00:35:57 -0500

    Update simple form validation

--- client/src/components/expenseForm/ExpenseForm.js
+++ client/src/components/expenseForm/ExpenseForm.js
@@ -1,26 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  const [enteredCategory, setEnteredCategory] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const intialValues = { category: "", amount: "", date: "" };
+  //'0001-01-01'
+  const [formValues, setFormValues] = useState({});
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const categoryHandler = (e) => {
-    setEnteredCategory(e.target.value);
-  };
-  const amountHandler = (e) => {
-    setEnteredAmount(e.target.value);
-  };
-  const dateHandler = (e) => {
-    setEnteredDate(e.target.value);
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    // use the variable "name" by wrapping the "name" with []
   };
 
-  const inputResetHandler = () => {
-    setEnteredCategory("");
-    setEnteredAmount("");
-    setEnteredDate("");
+  const validateForm = (values) => {
+    let errors = {};
+    const today = new Date();
+    const selectedDateString = values.date;
+    const selectedDate = new Date(selectedDateString);
+
+    // Category must be at least 2 characters long
+    // Amount must be numeric value (limit?)
+    // Date must be between 2000 year today + 1
+
+    if (values.category.length < 2) {
+      errors.category = "Category must be at least 2 characters long.";
+      console.log("Category is not valid");
+    }
+
+    if (values.amount <= 0) {
+      errors.amount = "Amount must be more than zero.";
+      console.log("Amount is not valid");
+    }
+
+    if (selectedDate > today) {
+      errors.date = "Date cannot be greater than today.";
+      console.log("Date is not valid");
+    }
+
+    return errors;
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setFormErrors(validateForm(formValues));
+    console.log(formValues);
+    setIsSubmitted(true);
   };
 
   async function postData(url = "", data = {}) {
@@ -34,24 +61,27 @@ const ExpenseForm = (props) => {
     });
   }
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-
-    console.log(
-      `Data input => ${enteredCategory} / ${enteredAmount} / ${enteredDate}`
-    );
-
+  const submitForm = () => {
     const url = "http://127.0.0.1:5000/post_expense";
     const data = {
-      enteredCategory: enteredCategory.trim(),
-      enteredAmount: enteredAmount.trim(),
-      enteredDate: enteredDate,
+      enteredCategory: formValues.category,
+      enteredAmount: formValues.amount,
+      enteredDate: formValues.date,
     };
+
+    console.log(data);
 
     postData(url, data).then((response) => console.log(response));
     props.fetchData();
-    inputResetHandler();
   };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmitted) {
+      submitForm();
+    } else {
+      console.log(formErrors);
+    }
+  }, [formErrors]);
 
   return (
     <div className="new-expense">
@@ -61,26 +91,32 @@ const ExpenseForm = (props) => {
             <label>Category</label>
             <input
               type="text"
-              value={enteredCategory}
-              onChange={categoryHandler}
+              id="category"
+              name="category"
+              value={formValues.category}
+              onChange={onChangeHandler}
             ></input>
           </div>
           <div className="expense_form_group">
             <label>Amount</label>
             <input
-              type="text"
+              type="number"
+              id="amount"
+              name="amount"
               min="0.01"
               step="0.01"
-              value={enteredAmount}
-              onChange={amountHandler}
+              value={formValues.amount}
+              onChange={onChangeHandler}
             ></input>
           </div>
           <div className="expense_form_group">
             <label>Date</label>
             <input
               type="date"
-              value={enteredDate}
-              onChange={dateHandler}
+              id="date"
+              name="date"
+              value={formValues.date}
+              onChange={onChangeHandler}
             ></input>
           </div>
         </div>
